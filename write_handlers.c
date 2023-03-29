60418a05a72f861a56500b5e7cf8c931e3a21650
LEBOHANG MOKOENA <63128420+MOKOENA9909@users.noreply.github.com>
2023-03-29 05:14:29 +0200

    Update write_handlers.c
    Update

--- write_handlers.c
+++ write_handlers.c
@@ -3,8 +3,8 @@
 /**
 * handle_write_char - Prints a string
 * @c: char types.
-* @buffer: Buffer array to handle print
-* @flags: Calculates active flags.
+* @buffer: buffer array to handle print
+* @flags: calculates active flags.
 * @width: get width.
 * @precision: precision specifier
 * @size: Size specifier
@@ -52,19 +52,19 @@ return (write(1, &buffer[0], 1));
 int write_number(int is_negative, int ind, char buffer[],
 int flags, int width, int precision, int size)
 {
-int length = BUFF_SIZE - ind - 1;
-char padd = ' ', extra_ch = 0;
-UNUSED(size);
-if ((flags & F_ZERO) && !(flags & F_MINUS))
-padd = '0';
-if (is_negative)
-extra_ch = '-';
-else if (flags & F_PLUS)
-extra_ch = '+';
-else if (flags & F_SPACE)
-extra_ch = ' ';
-return (write_num(ind, buffer, flags, width, precision,
-length, padd, extra_ch));
+  int length = BUFF_SIZE - ind - 1;
+  char padd = ' ', extra_ch = 0;
+  UNUSED(size);
+  if ((flags & F_ZERO) && !(flags & F_MINUS))
+  padd = '0';
+  if (is_negative)
+  extra_ch = '-';
+  else if (flags & F_PLUS)
+  extra_ch = '+';
+  else if (flags & F_SPACE)
+  extra_ch = ' ';
+  return (write_num(ind, buffer, flags, width, precision,
+  length, padd, extra_ch));
 }
 /**
 * write_num - Write a number using a bufffer
